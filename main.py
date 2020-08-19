e695a6026bca59da84068aaa208d321f8ea0b544
Sodment <57872802+Sodment@users.noreply.github.com>
2020-08-19 16:58:17 +0200

    Its a lot more complicated and better now

--- main.py
+++ main.py
@@ -1,52 +1,65 @@
-import pygame as game
+import pygame
 import pygame.freetype
 import pygame_widgets
-import cmath as cmath
-import time as time
-import sys as system
+import cmath
+import time
+import sys
 import _collections
+Vector = pygame.math.Vector2
 
+# initialize pygame
+pygame.init()
+FPS = 60  # frames per second
+fps_clock = pygame.time.Clock()
+# set up the window
+WIDTH = 1280
+HEIGHT = 800
+DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('ATOMOWO!')
+FONT = pygame.font.Font(None, 32)
+# RGB colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-def rysuj_ramke(surface, szerokosc , wysokosc):
-    ramka = game.draw.rect(surface, (140, 163, 163), (30, 30, szerokosc, wysokosc), 5)
-    surface.fill((198, 210, 209), (35, 35, szerokosc-10, wysokosc-10))
-    return ramka
+class Atom(pygame.sprite.Sprite):
 
-def rysuj_okno(szerokosc, wysokosc):
-    screen = game.display.set_mode((szerokosc, wysokosc))
-    screen.fill((72, 79, 79))
-    return screen
+    def __init__(self, radius, mass, surface, width, heigth):
+        self.atom_image = pygame.Surface((radius, radius))
+        pygame.draw.circle(surface, (157, 123, 133), (width, heigth), radius)
+        self.original_atom = self.atom_image
 
-def rysuj_atomy(surface, ramka, wysokosc, szerokosc):
-    promien = 15
-    for i in range(szerokosc):
-        for j in range(wysokosc):
-            if game.Rect.collidepoint(ramka, i, j) and i % 100 == 0 and j % 100 == 0:
-                game.draw.circle(surface, (157, 123, 133), (i, j), promien)
-            else:
-                continue
+class Container(pygame.sprite.Sprite):
+
+    def __init__(self, width, height):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((width, height))
+        self.image.fill((157, 123, 133))
+        pygame.draw.rect(self.image, (140, 163, 163), ((0, 0), (width, height)), 5)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH//3, HEIGHT//2)
+        self.position = (30, 30)
+
 
 
 
 def main():
-    game.init()
-    screen = rysuj_okno(1280, 820)
-    font = game.freetype.SysFont(None, 32)
-    clock = game.time.Clock()
-    ramka = rysuj_ramke(screen, 580, 720)
-    slider = pygame_widgets.Slider(screen, 100, 100, 800, 40, min=0, max=99, step=1)
-    rysuj_atomy(screen, ramka, 1280, 820)
-
-
-### Wychodzenie z gry ###
+    all_sprites = pygame.sprite.Group()
+    container = Container(600, 600)
+    all_sprites.add(container)
+    ### GAME LOOP ###
     while True:
-        events = game.event.get()
+        events = pygame.event.get()
         for e in events:
-            if e.type == game.QUIT:
+            if e.type == pygame.QUIT:
                 return 0
-        #slider.listen(events)
-        #slider.draw()
-        game.display.update()
+        DISPLAY.fill((140, 163, 163))
+        all_sprites.update()
+        all_sprites.draw(DISPLAY)
+        pygame.display.update()
+        fps_clock.tick(FPS)
 
 if __name__ == '__main__':
     main()
